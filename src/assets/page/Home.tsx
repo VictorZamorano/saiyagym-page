d377d20c87feed31c190a56d40fad12f8fdad8b6
Victor Zamorano <vzamoranoguerra@gmail.com>
2023-11-12 12:13:14 -0300

    breakpoints fix in home

--- src/assets/page/Home.tsx
+++ src/assets/page/Home.tsx
@@ -5,16 +5,6 @@ import Carrousel from '../component/Carrousel';
 
 
 export default function HomePage() {
-/* const heroImg = "./public/heroImg.jpg"
-
-  const bgImage = {
-    heroContainer:{
-      height: '35vh',
-      width: '100%',
-      backgroundImage: 'url('+heroImg+')',
-      backgroundSize: 'cover'
-    }
-  } */
 
   const Img = styled('img')({
     margin: 'auto',
@@ -37,18 +27,8 @@ export default function HomePage() {
     textAlign: 'justify'
   }
 
-  const strokeText = {
-  webkitTextStrokeWidth: '1px',
-  webkitTextStrokeColor:' black'
-  }
-
   return (
     <>
-        {/* <img className="sayagym-banner" src="./public/sayagym-banner.png" alt="sayagym-banner" /> */}
-        {/* <img alt="sayagym-banner" src="./public/heroImg.jpg" style={{width: "100%", height: '40vh', objectFit: 'cover'}}/> */}
-        {/* <Grid container justifyContent={'flex-end'} style={bgImage.heroContainer}>
-          <Typography sx={{color: theme.palette.text.secondary}}>BATALLA SAIYAGYM</Typography>
-        </Grid> */}
         <Grid marginTop={8} marginBottom={5}>
           <Box>
             <Carrousel />
@@ -72,37 +52,41 @@ export default function HomePage() {
         borderRadius: {xs: 0, md: '20px'},  
 }} 
     >
-        <Grid className='schedulesImg' container spacing={2} display="flex" justifyContent="center" alignItems="center">
+        <Grid className='schedulesImg' container justifyContent="center">
           <Grid margin={1}>
-          <Typography variant='h3' sx={{...strokeText}}>Horarios</Typography>
+          <Typography variant='h3'>Horarios</Typography>
           </Grid>
-          <Grid item xs={12} md={3}>
-            <Img src="/schedules1.png" alt="schudeles1" sx={{...whiteBorder}}/>
-          </Grid>
-          <Grid item xs={12} md={3}>
-            <Img src="/schedules2.png" alt="schudeles2" sx={{...whiteBorder}}/>
-          </Grid>
-          <Grid item xs={12} md={3}>
-            <Box sx={{...whiteBorder, p: 1, color: '#fff'}}>
-              <Box sx={{...centerTitle}}>
-                <Typography variant='h5' color={'secondary'} fontWeight={'bold'}>Horario Fijo:</Typography> 
-                <Box sx={{...justifyText}}>
-                    <Typography variant='body1'>Valor de $30.000 mensual. Clases de dos horas y se debe inscribir en uno de los dos bloques desde las 07:00 AM a 21:00 PM</Typography>
+          <Grid container spacing={2} justifyContent="center" alignItems="center">
+            <Grid item xs={12} sm={6} md={5} lg={3}>
+              <Img src="/schedules1.png" alt="schudeles1" sx={{...whiteBorder}}/>
+            </Grid>
+            <Grid item xs={12} sm={6} md={5} lg={3}>
+              <Img src="/schedules2.png" alt="schudeles2" sx={{...whiteBorder}}/>
+            </Grid>
+            <Grid item xs={12} sm={6} md={5} lg={3}>
+              <Box sx={{...whiteBorder, p: 1, color: '#fff'}}>
+                <Box sx={{display: 'flex', flexWrap: 'wrap', alignContent: 'stretch', height: 509,}}>
+                  <Box sx={{...centerTitle}}>
+                    <Typography variant='h5' color={'secondary'} fontWeight={'bold'}>Horario Fijo:</Typography> 
+                    <Box sx={{...justifyText}}>
+                        <Typography variant='body1'>Valor de $30.000 mensual. Clases de dos horas y se debe inscribir en uno de los dos bloques desde las 07:00 AM a 21:00 PM</Typography>
+                    </Box>
+                  </Box>
+                  <Box sx={{...centerTitle}}>
+                    <Typography variant='h5' color={'secondary'} fontWeight={'bold'}>Horario Libre 1: Cupos limitados.</Typography>
+                    <Box sx={{...justifyText}}>
+                      <Typography variant='body1'>Valor de $40.000 mensual. Tres veces por semana, días y horarios libres pero debe realizarse en los bloques de 2 horas indicados en las imagenes.</Typography>
+                    </Box>
+                  </Box>
+                  <Box sx={{...centerTitle}}>
+                    <Typography variant='h5'color={'secondary'} fontWeight={'bold'}>Horario Libre 2: Cupos limitados</Typography> 
+                    <Box sx={{...justifyText}}>
+                      <Typography variant='body1'>Valor de $50.000 mensual. Seis veces por semana y horarios libres pero debe realizarse en los bloques de 2 horas indicados en las imagenes.</Typography>
+                    </Box>
+                  </Box>
                 </Box>
               </Box>
-              <Box sx={{...centerTitle}}>
-                <Typography variant='h5' color={'secondary'} fontWeight={'bold'}>Horario Libre 1: Cupos limitados.</Typography>
-                <Box sx={{...justifyText}}>
-                  <Typography variant='body1'>Valor de $40.000 mensual. Tres veces por semana, días y horarios libres pero debe realizarse en los bloques de 2 horas indicados en las imagenes.</Typography>
-                </Box>
-              </Box>
-              <Box sx={{...centerTitle}}>
-                <Typography variant='h5'color={'secondary'} fontWeight={'bold'}>Horario Libre 2: Cupos limitados</Typography> 
-                <Box sx={{...justifyText}}>
-                  <Typography variant='body1'>Valor de $50.000 mensual. Seis veces por semana y horarios libres pero debe realizarse en los bloques de 2 horas indicados en las imagenes.</Typography>
-                </Box>
-              </Box>
-            </Box>
+            </Grid>
           </Grid>
         </Grid>
       </Paper>
